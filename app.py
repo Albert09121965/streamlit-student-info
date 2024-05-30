ffde2c24315d74297ba97bf074a5de3b3fed35ea
Albert09121965 <albertparra1@gmail.com>
2024-05-30 11:56:13 +0200

    Add app.py to local repo

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+import streamlit as st
+
+st.title("Student Information yeedsgsg") # Title of the app
+student_name = st.text_input("Enter the student's name:") # Text input for student's name
+student_age = st.slider("Select the student's age:", 1, 100) # Slider for student's age
+
+# Button to display the input text and age
+if st.button("Display Information"):
+    st.write("Student's name: ", student_name)
+    st.write("Student's age: ", student_age)
