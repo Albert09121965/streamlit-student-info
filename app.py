4f67ad237275750a02848091c09c155d3673367c
Albert09121965 <albertparra1@gmail.com>
2024-05-30 12:25:16 +0200

    Reload again

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # app.py
 import streamlit as st
 
-st.title("Student Information yeedsgsg") # Title of the app
+st.title("Student Information") # Title of the app
 student_name = st.text_input("Enter the student's name:") # Text input for student's name
 student_age = st.slider("Select the student's age:", 1, 100) # Slider for student's age
 
