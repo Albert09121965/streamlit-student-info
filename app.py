03b2b7068b61211d216fa25b6937ea5b0cf5eba0
Albert09121965 <albertparra1@gmail.com>
2024-05-30 14:07:41 +0200

    add dataframe and new variable

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import streamlit as st
 st.title("Student Information") # Title of the app
 student_name = st.text_input("Enter the student's name:") # Text input for student's name
 student_age = st.slider("Select the student's age:", 1, 100) # Slider for student's age
+student_age =int(student_age) # transformer en integer
+df_age = pd.DataFrame({'âge': list(range(student_age), student_age+11)),'second column': np.arange(10, 101, 10))
 
 # Button to display the input text and age
 if st.button("Display Information"):
@@ -11,3 +13,4 @@ if st.button("Display Information"):
     st.write("Student's age: ", student_age)
     if int(student_age) < 12:
         st.write(student_name, "un mensonge, pouvez-vous SVP saisir votre âge")
+    st.write(df_age)
